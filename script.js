4be5f53d46e99f264cb652d7421f011510058596
Mikaella Antonette Tayoto <mikaellatayoto04@gmail.com>
2024-09-23 03:21:21 +0800

    add object

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ var cat = {
         console.log("I want a cat, that is color ".concat(this.color, ", with a lifespan \n                    of ").concat(this.lifespan, ", and a ").concat(this.gender, ". \n Cat's are \n                    part of a ").concat(this.animal, " group and they can help us with \n                    stress that boost our mental health."));
     }
 };
+var goldfish = {
+    animal: Animals.FISH,
+    color: "golden",
+    lifespan: "1 year",
+    gender: "female",
+    print: function () {
+        console.log("When I was a kid, I have a ".concat(this.animal, " that's a goldfish, \n            it has color of ").concat(this.color, " it lived around for almost ").concat(this.lifespan, ". \n            I think it's a ").concat(this.gender, "."));
+    }
+};
